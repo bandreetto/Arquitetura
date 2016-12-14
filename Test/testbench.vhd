536351f8c232d1091e2da75c06eb9debd57f5b03
bandreetto <bandreetto@hotmail.com>
2016-12-14 21:10:35 -0200

    Corrigindo erros de caractere

--- Test/testbench.vhd
+++ Test/testbench.vhd
@@ -1,50 +1,46 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all; use IEEE.STD_LOGIC_UNSIGNED.all;
+library IEEE; 
+use IEEE.STD_LOGIC_1164.all; use IEEE.NUMERIC_STD_UNSIGNED.all;
 
 entity testbench is
 end;
 
 architecture test of testbench is
+  component top
+    port(clk, reset:           in  STD_LOGIC;
+         writedata, dataadr:   out STD_LOGIC_VECTOR(31 downto 0);
+         memwrite:             out STD_LOGIC);
+  end component;
+  signal writedata, dataadr:    STD_LOGIC_VECTOR(31 downto 0);
+  signal clk, reset,  memwrite: STD_LOGIC;
+begin
 
-    component top
-        port(clk, reset: in STD_LOGIC;
-            writedata, dataadr: out STD_LOGIC_VECTOR(31 downto 0);
-            memwrite: out STD_LOGIC);
-    end component;
+  -- instantiate device to be tested
+  dut: top port map(clk, reset, writedata, dataadr, memwrite);
 
-    signal writedata, dataadr: STD_LOGIC_VECTOR(31 downto 0);
-    signal clk, reset, memwrite: STD_LOGIC;
+  -- Generate clock with 10 ns period
+  process begin
+    clk <= '1';
+    wait for 5 ns; 
+    clk <= '0';
+    wait for 5 ns;
+  end process;
 
-    begin
-        — — instantiate device to be tested
-        dut: top port map (clk, reset, writedata, dataadr, memwrite);
+  -- Generate reset for first two clock cycles
+  process begin
+    reset <= '1';
+    wait for 22 ns;
+    reset <= '0';
+    wait;
+  end process;
 
-        — — Generate clock with 10 ns period
-        process begin
-            clk <= '1';
-            wait for 5 ns;
-            clk <= '0';
-            wait for 5 ns;
-        end process;
-
-        — — Generate reset for first two clock cycles
-        process begin
-            reset <= '1';
-            wait for 22 ns;
-            reset <= '0';
-            wait;
-        end process;
-
-        — — check that 7 gets written to address 84
-        — — at end of program
-        process (clk) begin
-            if (clk'event and clk = '0' and memwrite = '1') then
-                if (conv_integer(dataadr) = 84 and conv_integer
-                    (writedata) = 7) then
-                    report "Simulation succeeded";
-                elsif (dataadr /= 80) then
-                    report "Simulation failed";
-                end if;
-            end if;
-        end process;
+  -- check that 7 gets written to address 84 at end of program
+  process (clk) begin
+    if (clk'event and clk = '0' and memwrite = '1') then
+      if (to_integer(dataadr) = 84 and to_integer(writedata) = 7) then 
+        report "NO ERRORS: Simulation succeeded" severity failure;
+      elsif (dataadr /= 80) then 
+        report "Simulation failed" severity failure;
+      end if;
+    end if;
+  end process;
 end;
