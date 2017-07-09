88914ab4f5110037a928a9d7b1592b2af77fb498
giovanni-gardusi <giovanni.gardusi@gmail.com>
2017-07-09 14:18:09 -0300

    Funcoes transform e byte_reverse implementadas em SystemC

--- sha_sysc.cpp
+++ sha_sysc.cpp
@@ -1,101 +1,109 @@
 #include "systemc.h"
 #include "sha_sysc.h"
 
-SC_MODULE (sha_transform) {
+SC_MODULE (sha_systemc) {
 
+	sc_in<bool> exec;
+
+	//Transform
 	sc_in<LONG> digest[5];
 	sc_in<LONG> data[16];
 
 	sc_out<LONG> digest_out[5];
 
-	void transform() {
-		int i;
-		LONG temp, A, B, C, D, E, W[80];
-
-		for (i = 0; i < 16; ++i) {
-			W[i] = data[i].read();
-		}
-		for (i = 16; i < 80; ++i) {
-			W[i] = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16];
-#ifdef USE_MODIFIED_SHA
-			W[i] = ROT32(W[i], 1);
-#endif /* USE_MODIFIED_SHA */
-		}
-		A = digest[0].read();
-		B = digest[1].read();
-		C = digest[2].read();
-		D = digest[3].read();
-		E = digest[4].read();
-
-#ifdef UNROLL_LOOPS
-		FUNC(1, 0); FUNC(1, 1); FUNC(1, 2); FUNC(1, 3); FUNC(1, 4);
-		FUNC(1, 5); FUNC(1, 6); FUNC(1, 7); FUNC(1, 8); FUNC(1, 9);
-		FUNC(1,10); FUNC(1,11); FUNC(1,12); FUNC(1,13); FUNC(1,14);
-		FUNC(1,15); FUNC(1,16); FUNC(1,17); FUNC(1,18); FUNC(1,19);
-
-		FUNC(2,20); FUNC(2,21); FUNC(2,22); FUNC(2,23); FUNC(2,24);
-		FUNC(2,25); FUNC(2,26); FUNC(2,27); FUNC(2,28); FUNC(2,29);
-		FUNC(2,30); FUNC(2,31); FUNC(2,32); FUNC(2,33); FUNC(2,34);
-		FUNC(2,35); FUNC(2,36); FUNC(2,37); FUNC(2,38); FUNC(2,39);
-
-		FUNC(3,40); FUNC(3,41); FUNC(3,42); FUNC(3,43); FUNC(3,44);
-		FUNC(3,45); FUNC(3,46); FUNC(3,47); FUNC(3,48); FUNC(3,49);
-		FUNC(3,50); FUNC(3,51); FUNC(3,52); FUNC(3,53); FUNC(3,54);
-		FUNC(3,55); FUNC(3,56); FUNC(3,57); FUNC(3,58); FUNC(3,59);
-
-		FUNC(4,60); FUNC(4,61); FUNC(4,62); FUNC(4,63); FUNC(4,64);
-		FUNC(4,65); FUNC(4,66); FUNC(4,67); FUNC(4,68); FUNC(4,69);
-		FUNC(4,70); FUNC(4,71); FUNC(4,72); FUNC(4,73); FUNC(4,74);
-		FUNC(4,75); FUNC(4,76); FUNC(4,77); FUNC(4,78); FUNC(4,79);
-#else /* !UNROLL_LOOPS */
-		for (i = 0 ; i < 20; ++i) FUNC(1, i);
-		for (i = 20; i < 40; ++i) FUNC(2, i);
-		for (i = 40; i < 60; ++i) FUNC(3, i);
-		for (i = 60; i < 80; ++i) FUNC(4, i);
-#endif /* !UNROLL_LOOPS */
-		digest_out[0].write(digest[0].read() + A);
-		digest_out[1].write(digest[1].read() + B);
-		digest_out[2].write(digest[2].read() + C);
-		digest_out[3].write(digest[3].read() + D);
-		digest_out[4].write(digest[4].read() + E);
-	}
-
-	SC_CTOR(sha_transform) {
-		SC_METHOD(transform);
-	}
-};
-
-SC_MODULE (sha_byte_reverse) {
-
-	sc_in<LONG> buffer;
+	//Byte Reverse
+	sc_in<LONG> buffer[16];
 	sc_in<int> counter;
 
-	sc_out<LONG> buffer_out;
+	sc_out<LONG> buffer_out[16];
 
+	void transform() {
+		if (exec.read()) {
+			int i;
+			LONG temp, A, B, C, D, E, W[80];
+
+			for (i = 0; i < 16; ++i) {
+				W[i] = data[i].read();
+			}
+			for (i = 16; i < 80; ++i) {
+				W[i] = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16];
+#ifdef USE_MODIFIED_SHA
+				W[i] = ROT32(W[i], 1);
+#endif /* USE_MODIFIED_SHA */
+			}
+			A = digest[0].read();
+			B = digest[1].read();
+			C = digest[2].read();
+			D = digest[3].read();
+			E = digest[4].read();
+
+#ifdef UNROLL_LOOPS
+			FUNC(1, 0); FUNC(1, 1); FUNC(1, 2); FUNC(1, 3); FUNC(1, 4);
+			FUNC(1, 5); FUNC(1, 6); FUNC(1, 7); FUNC(1, 8); FUNC(1, 9);
+			FUNC(1,10); FUNC(1,11); FUNC(1,12); FUNC(1,13); FUNC(1,14);
+			FUNC(1,15); FUNC(1,16); FUNC(1,17); FUNC(1,18); FUNC(1,19);
+
+			FUNC(2,20); FUNC(2,21); FUNC(2,22); FUNC(2,23); FUNC(2,24);
+			FUNC(2,25); FUNC(2,26); FUNC(2,27); FUNC(2,28); FUNC(2,29);
+			FUNC(2,30); FUNC(2,31); FUNC(2,32); FUNC(2,33); FUNC(2,34);
+			FUNC(2,35); FUNC(2,36); FUNC(2,37); FUNC(2,38); FUNC(2,39);
+
+			FUNC(3,40); FUNC(3,41); FUNC(3,42); FUNC(3,43); FUNC(3,44);
+			FUNC(3,45); FUNC(3,46); FUNC(3,47); FUNC(3,48); FUNC(3,49);
+			FUNC(3,50); FUNC(3,51); FUNC(3,52); FUNC(3,53); FUNC(3,54);
+			FUNC(3,55); FUNC(3,56); FUNC(3,57); FUNC(3,58); FUNC(3,59);
+
+			FUNC(4,60); FUNC(4,61); FUNC(4,62); FUNC(4,63); FUNC(4,64);
+			FUNC(4,65); FUNC(4,66); FUNC(4,67); FUNC(4,68); FUNC(4,69);
+			FUNC(4,70); FUNC(4,71); FUNC(4,72); FUNC(4,73); FUNC(4,74);
+			FUNC(4,75); FUNC(4,76); FUNC(4,77); FUNC(4,78); FUNC(4,79);
+#else /* !UNROLL_LOOPS */
+			for (i = 0 ; i < 20; ++i) FUNC(1, i);
+			for (i = 20; i < 40; ++i) FUNC(2, i);
+			for (i = 40; i < 60; ++i) FUNC(3, i);
+			for (i = 60; i < 80; ++i) FUNC(4, i);
+#endif /* !UNROLL_LOOPS */
+			digest_out[0].write(digest[0].read() + A);
+			digest_out[1].write(digest[1].read() + B);
+			digest_out[2].write(digest[2].read() + C);
+			digest_out[3].write(digest[3].read() + D);
+			digest_out[4].write(digest[4].read() + E);
+		}
+	}
 
 //#ifdef LITTLE_ENDIAN
 	void byte_reverse() {
-		int i, count = counter.read();
-		BYTE ct[4], *cp;
+		if (exec.read()) {
+			int i, count;
+			BYTE ct[4], *cp;
+			LONG *data = (LONG *) malloc(sizeof(LONG) * 16);
 
-		count /= sizeof(LONG);
-		cp = (BYTE *) buffer.read();
-		for (i = 0; i < count; ++i) {
-			ct[0] = cp[0];
-			ct[1] = cp[1];
-			ct[2] = cp[2];
-			ct[3] = cp[3];
-			cp[0] = ct[3];
-			cp[1] = ct[2];
-			cp[2] = ct[1];
-			cp[3] = ct[0];
-			cp += sizeof(LONG);
+			count = counter.read()/sizeof(LONG);
+			for (int i = 0; i < 16; i++)
+				data[i] = buffer[i].read();
+			cp = (BYTE *) data;
+			for (i = 0; i < count; ++i) {
+				ct[0] = cp[0];
+				ct[1] = cp[1];
+				ct[2] = cp[2];
+				ct[3] = cp[3];
+				cp[0] = ct[3];
+				cp[1] = ct[2];
+				cp[2] = ct[1];
+				cp[3] = ct[0];
+				cp += sizeof(LONG);
+			}
+			data = (LONG *) cp;
+			for (int i = 0; i < 16; i++)
+				buffer_out[i].write(data[i]);
 		}
-		buffer_out.write((LONG) cp);
 	}
 //#endif /* LITTLE_ENDIAN */
 
-	SC_CTOR(sha_byte_reverse) {
+	SC_CTOR(sha_systemc) {
+		SC_METHOD(transform);
+			sensitive << exec;
 		SC_METHOD(byte_reverse);
+			sensitive << exec;
 	}
 };
--- sha_sysc_driver.cpp
+++ sha_sysc_driver.cpp
@@ -1,43 +1,84 @@
 #include "systemc.h"
 #include "sha_sysc.cpp"
 
+void print(char *tag, sc_signal<LONG> a[], int size) {
+	cout << tag;
+	for(int i = 0; i < size; i++) cout << (char)a[i].read();
+	cout << endl;
+}
+
 int sc_main(int argc, char* argv[])
 {
+	sc_report_handler::set_actions("/IEEE_Std_1666/deprecated", SC_DO_NOTHING);
+
+	//Exec
+	sc_signal<bool> exec;
+
+	//Transform
 	sc_signal<LONG> digest[5];
 	sc_signal<LONG> data[16];
 
 	sc_signal<LONG> digest_out[5];
 
-	sha_transform transform("TRANSFORM");
+	//Byte Reverse
+	sc_signal<LONG> buffer[16];
+	sc_signal<int> counter;
 
-	char texto[16] = "Hello World!";
+	sc_signal<LONG> buffer_out[16];
+
+	//Associating signals to ports
+	sha_systemc coprocessor("COPROCESSOR");
+	coprocessor.exec(exec);
+	coprocessor.counter(counter);
 	for(int i = 0; i < 16; i++) {
-		transform.data[i](data[i]);
-		data[i] = (LONG) texto[i];
+		coprocessor.data[i](data[i]);
+		coprocessor.buffer[i](buffer[i]);
+		coprocessor.buffer_out[i](buffer_out[i]);
 	}
 	for(int i = 0; i < 5; i++) {
-		transform.digest[i](digest[i]);
-		transform.digest_out[i](digest_out[i]);
+		coprocessor.digest[i](digest[i]);
+		coprocessor.digest_out[i](digest_out[i]);
 	}
+
+	//Tracing
+//	sc_trace_file *wf = sc_create_vcd_trace_file("sc_start");
+//	sc_trace(wf, coprocessor.data, "data");
+//	sc_trace(wf, coprocessor.digest, "digest");
+//	sc_trace(wf, coprocessor.digest_out, "digest_out");
+//	sc_trace(wf, coprocessor.buffer, "buffer");
+//	sc_trace(wf, coprocessor.buffer_out, "buffer_out");
+
+	sc_start(0);
+	char texto[17] = "Teste do SystemC";
+	for(int i = 0; i < 16; i++) {
+		data[i] = (LONG) texto[i];
+		buffer[i] = i+48;
+	}
+
 	digest[0] = 0x67452301L;
 	digest[1] = 0xefcdab89L;
 	digest[2] = 0x98badcfeL;
 	digest[3] = 0x10325476L;
 	digest[4] = 0xc3d2e1f0L;
 
-	// Open VCD file
-//	sc_trace_file *wf = sc_create_vcd_trace_file("counter");
-	// Dump the desired signals
-//	sc_trace(wf, data, "data");
-//	sc_trace(wf, digest, "digest");
-//	sc_trace(wf, digest_out, "digest_out");
+	counter = 8*sizeof(LONG);
 
-	cout << "TESTE DE CODIGO\nDADOS: ";
-	for(int i = 0; i < 16; i++) {
-		cout << data[i].read();
-	}
-	cout << "\n";
-	transform.transform();
+	exec = 0;
+	sc_start(1);
+	print("DATA: ", data, 16);
+	print("DIGEST: ", digest, 5);
+	print("DIGEST_OUT: ", digest_out, 5);
+	print("BUFFER: ", buffer, 16);
+	print("BUFFER_OUT: ", buffer_out, 16);
 
+	exec = 1;
+	sc_start(1);
+	print("DATA: ", data, 16);
+	print("DIGEST: ", digest, 5);
+	print("DIGEST_OUT: ", digest_out, 5);
+	print("BUFFER: ", buffer, 16);
+	print("BUFFER_OUT: ", buffer_out, 16);
+
+//	sc_close_vcd_trace_file(wf);
 	return 0;
 }
